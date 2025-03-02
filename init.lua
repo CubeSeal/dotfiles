d0efc64bffe9bc4663fa68c71b745fa69756cf36
landseal <kenndesilva1@gmail.com>
2025-03-02 11:27:39 +1100

    Add hover diagnostics instead of virtual text.

--- init.lua
+++ init.lua
@@ -57,9 +57,17 @@ cmp.setup{
     },
 }
 
-vim.diagnostic.config { virtual_text = true }
-
 map('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+
+-- You will likely want to reduce updatetime which affects CursorHold
+-- note: this setting is global and should be set only once
+vim.o.updatetime = 250
+vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+  group = vim.api.nvim_create_augroup("float_diagnostic", { clear = true }),
+  callback = function ()
+    vim.diagnostic.open_float(nil, {focus=false})
+  end
+})
 
 -- Individual LSP setups
 lsp_config = require('lspconfig')
