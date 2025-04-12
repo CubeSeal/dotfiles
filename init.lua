084a9114b6bf53cda7877e997659a920c16cdea2
landseal <kenndesilva1@gmail.com>
2025-04-12 18:47:15 +1000

    Inline AI.lua.

--- init.lua
+++ init.lua
@@ -6,9 +6,9 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.updatetime = 250
 vim.opt.timeoutlen = 300
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4 
 vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
 vim.opt.colorcolumn = "120"
 vim.wo.relativenumber = true
 vim.g.netrw_bufsettings = 'noma nomod nu rnu nobl nowrap ro'
@@ -128,11 +128,71 @@ map('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 map('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
 -- AI:
-local AI_FLAG = 1
+-- Copilot
+require('copilot').setup({
+  suggestion = {
+    enabled = true,
+    auto_trigger = true,
+    hide_during_completion = true,
+    debounce = 75,
+    keymap = {
+      accept = "<M-l>",
+      accept_word = false,
+      accept_line = false,
+      next = "<M-]>",
+      prev = "<M-[>",
+      dismiss = "<C-]>",
+      }
+    },
+})
 
-if AI_FLAG == 1 then 
-    require('config.AI')
-end
+-- Code completions
+require("codecompanion").setup({
+  adapters = {
+    gemini = function()
+      return require("codecompanion.adapters").extend("gemini", {
+      name = "Gemini 2.5",
+      scheme = {
+        model = {
+          default = "gemini-2.5-pro-exp-03-25",
+        }
+      },
+      env = {
+        api_key = "cmd:cat ~/gemini.apikey"
+      },
+    })
+    end,
+  },
+  strategies = {
+    chat = {
+      adapter = "gemini",
+    },
+    inline = {
+      adapter = "gemini",
+    },
+    cmd = {
+      adapter = "gemini",
+    },
+  },
+  display = {
+    action_palette = {
+      width = 95,
+      height = 10,
+      prompt = "Prompt ", -- Prompt used for interactive LLM calls
+      provider = "telescope", -- default|ttelescopeelescope|mini_pick
+      opts = {
+        show_default_actions = true, -- Show the default actions in the action palette?
+        show_default_prompt_library = true, -- Show the default prompt library in the action palette?
+      },
+    },
+  },
+})
+
+map({ "n", "v" }, "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
+map({ "n", "v" }, "<LocalLeader>a", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true })
+map("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
+
+vim.cmd([[cab cc CodeCompanion]])
 
 -- Render Markdown
 require('render-markdown').setup({
