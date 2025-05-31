adc420cc05d5cc2e3c46f112ea3ddd1110770d30
landseal <kenndesilva1@gmail.com>
2025-05-31 13:25:22 +1000

    Add fold markers and change tab-size to 2.

--- init.lua
+++ init.lua
@@ -1,32 +1,35 @@
--- Basic stuff
-vim.opt.number = true
-vim.opt.mouse = 'a'
-vim.opt.wrap = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.updatetime = 250
-vim.opt.timeoutlen = 300
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.colorcolumn = "120"
+-- vim:fileencoding=utf-8:foldmethod=marker
+local opts = { noremap = true, silent = true }
+local map = vim.keymap.set
+local opt = vim.opt
+
+-- Standard Vim Options {{{1
+opt.number = true
+opt.mouse = 'a'
+opt.wrap = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.updatetime = 250
+opt.timeoutlen = 300
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.tabstop = 2
+opt.colorcolumn = "120"
 vim.wo.relativenumber = true
 vim.g.netrw_bufsettings = 'noma nomod nu rnu nobl nowrap ro'
-vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+opt.completeopt = {'menu', 'menuone', 'noselect'}
 
--- Folds
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-vim.opt.foldlevelstart = 99
+-- Folds {{{2
+opt.foldmethod = "expr"
+opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+opt.foldlevelstart = 99
 
--- Remaps
+-- Leader {{{2
 vim.g.mapleader = ' '
 vim.g.maplocalleader = '\\'
-local opts = { noremap = true, silent = true }
-map = vim.keymap.set
 
--- Autocommands
--- Highlight on Yank
+-- Autocommands {{{1
+-- Highlight on Yank {{{2
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
@@ -35,66 +38,68 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Diagnostics {{{2
+vim.diagnostic.config{virtual_lines = true}
 -- Diagnostic Floating window:
 -- You will likely want to reduce updatetime which affects CursorHold
-vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-  group = vim.api.nvim_create_augroup("float_diagnostic", { clear = true }),
-  callback = function ()
-    vim.diagnostic.open_float(nil, {focus=false})
-  end
-})
+-- vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+--   group = vim.api.nvim_create_augroup("float_diagnostic", { clear = true }),
+--   callback = function ()
+--     vim.diagnostic.open_float(nil, {focus=false})
+--   end
+-- })
 
--- Configs
+-- Configs {{{1
 require("config.lazy")
 
--- LSP
+-- LSP {{{2
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local cmp = require('cmp')
 local select_opts = {behavior = cmp.SelectBehavior.Select}
 
 cmp.setup{
-    sources = cmp.config.sources { 
-        { name = 'nvim_lsp' },
-        { name = 'render-markdown'},
-        { name = "buffer" },
-        { name = 'path' }
-    },
-    window = {
-      documentation = cmp.config.window.bordered()
-    },
-    formatting = {
-        fields = {'menu', 'abbr', 'kind'},
-        format = function(entry, item)
-          local menu_icon = {
-            nvim_lsp = 'λ',
-            buffer = 'Ω',
-            path = '🖫',
-          }
+  sources = cmp.config.sources { 
+    { name = 'nvim_lsp' },
+    { name = 'render-markdown'},
+    { name = "buffer" },
+    { name = 'path' }
+  },
+  window = {
+    documentation = cmp.config.window.bordered()
+  },
+  formatting = {
+    fields = {'menu', 'abbr', 'kind'},
+    format = function(entry, item)
+      local menu_icon = {
+        nvim_lsp = 'λ',
+        buffer = 'Ω',
+        path = '🖫',
+      }
 
-          item.menu = menu_icon[entry.source.name]
-          return item
-        end,
-    },
-    mapping = {
-        ['<C-p>'] = cmp.mapping.select_prev_item(select_opts),
-        ['<C-n>'] = cmp.mapping.select_next_item(select_opts),
-        ['<CR>'] = cmp.mapping.confirm({select = false}),
-        ['<Tab>'] = cmp.mapping.confirm({select = true}),
-    },
+      item.menu = menu_icon[entry.source.name]
+      return item
+    end,
+  },
+  mapping = {
+    ['<C-p>'] = cmp.mapping.select_prev_item(select_opts),
+    ['<C-n>'] = cmp.mapping.select_next_item(select_opts),
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+    ['<Tab>'] = cmp.mapping.confirm({select = true}),
+  },
 }
 
--- Individual LSP setups
+-- Individual LSP setups {{{3
 lsp_config = require('lspconfig')
 
 lsp_config.hls.setup{
-    capabilities = capabilities, 
-}
- 
-lsp_config.pyright.setup{
-    capabilities = capabilities, 
+  capabilities = capabilities, 
 }
 
--- Treesitter
+lsp_config.pyright.setup{
+  capabilities = capabilities, 
+}
+
+-- Treesitter {{{2
 require'nvim-treesitter.configs'.setup { 
   highlight = { 
     enable = true, 
@@ -104,31 +109,31 @@ require'nvim-treesitter.configs'.setup {
   }, 
 }
 
--- Themes
+-- Themes {{{2
 require('kanagawa').setup({
-    colors = {
-        theme = {
-            all = {
-                ui = {
-                    bg_gutter = "none"
-                }
-            }
+  colors = {
+    theme = {
+      all = {
+        ui = {
+          bg_gutter = "none"
         }
-    }, 
-    transparent = true,
-    theme = "dragon",
+      }
+    }
+  }, 
+  transparent = true,
+  theme = "dragon",
 })
 vim.cmd[[colorscheme kanagawa]]
 
--- Telescope keybindings
+-- Telescope keybindings {{{2
 local builtin = require('telescope.builtin')
 map('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 map('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 map('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 map('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
--- AI:
--- Copilot
+-- AI {{{2
+-- Copilot {{{3
 require('copilot').setup({
   suggestion = {
     enabled = true,
@@ -142,25 +147,25 @@ require('copilot').setup({
       next = "<M-]>",
       prev = "<M-[>",
       dismiss = "<C-]>",
-      }
-    },
+    }
+  },
 })
 
--- Code completions
+-- Code Companion {{{3
 require("codecompanion").setup({
   adapters = {
     gemini = function()
       return require("codecompanion.adapters").extend("gemini", {
-      name = "Gemini 2.5",
-      scheme = {
-        model = {
-          default = "gemini-2.5-pro-exp-03-25",
-        }
-      },
-      env = {
-        api_key = "cmd:cat ~/gemini.apikey"
-      },
-    })
+        name = "Gemini 2.5",
+        scheme = {
+          model = {
+            default = "gemini-2.5-pro-exp-03-25",
+          }
+        },
+        env = {
+          api_key = "cmd:cat ~/gemini.apikey"
+        },
+      })
     end,
   },
   strategies = {
@@ -196,7 +201,7 @@ vim.cmd([[cab cc CodeCompanion]])
 
 -- Render Markdown
 require('render-markdown').setup({
-    file_types = {'markdown', 'codecompanion'},
-    render_modes = { 'n', 'c', 't'},
-    sign = {enabled = false},
+  file_types = {'markdown', 'codecompanion'},
+  render_modes = { 'n', 'c', 't'},
+  sign = {enabled = false},
 })
