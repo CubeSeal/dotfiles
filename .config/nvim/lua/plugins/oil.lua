4ddd6fa686c006377b97f413cc770dd260cac005
landseal <kenndesilva1@gmail.com>
2025-06-14 13:04:12 +1000

    Config oil.

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -8,4 +8,15 @@ return {
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
+  config = function()
+    require("oil").setup({
+      -- Your custom configuration goes here
+      view_options = {
+        show_hidden = true, -- Show hidden files
+      }
+    })
+
+    -- Key mappings for oil.nvim
+    vim.keymap.set("n", "-", require("oil").open, { desc = "Open Oil" })
+  end
 }
