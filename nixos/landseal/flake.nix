ef0f99e4baf0c07c210af3847ae87b5a747703e5
landseal <kenndesilva1@gmail.com>
2025-09-20 19:05:06 +1000

    Add zsh autosuggestions and syntaxhighlighting.

--- nixos/landseal/flake.nix
+++ nixos/landseal/flake.nix
@@ -152,6 +152,8 @@
                   zsh = {
                     enable = true;
                     enableCompletion = true;
+                    autosuggestions.enable = true;
+                    syntaxHighlighting.enable = true;
                     ohMyZsh = {
                       enable = true;
                       plugins = [ "git" ];
