474f4b96e25ec5299a49e4e9f8fcc3dc80f8b2ef
CubeSeal <kenndesilva1@gmail.com>
2025-10-11 20:23:26 +1100

    Add gpg.

--- nixos/landseal/flake.nix
+++ nixos/landseal/flake.nix
@@ -202,15 +202,16 @@
                     enable = true;
                     enableZshIntegration = true;
                   };
+                  gnupg.agent = {
+                    enable = true;
+                    enableSSHSupport = true;
+                    pinentryPackage = pkgs.pinentry-gnome3;
+                  };
                 };
 
                 # Some programs need SUID wrappers, can be configured further or are
                 # started in user sessions.
                 # programs.mtr.enable = true;
-                # programs.gnupg.agent = {
-                #   enable = true;
-                #   enableSSHSupport = true;
-                # };
 
                 # Packages
                 # List packages installed in system profile. To search, run:
