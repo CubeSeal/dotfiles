b8385a69ab313b959344d2cc24156fd53199e7b3
CubeSeal <kenndesilva1@gmail.com>
2025-10-29 22:31:35 +1100

    Telescope hidden files.

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -5,6 +5,10 @@ return {
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
     local builtin = require('telescope.builtin')
+
+    local telescope = require('telescope')
+    telescope.setup { pickers = { find_files = { hidden = true } } }
+
     vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
     vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
