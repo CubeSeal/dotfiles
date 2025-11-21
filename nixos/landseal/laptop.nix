dbf27580fa90033f242ba6a46606d32fcc36b39c
CubeSeal <kenndesilva1@gmail.com>
2025-11-21 20:27:12 +1100

    Remove powertop since it can conflict with tlp.

--- nixos/landseal/laptop.nix
+++ nixos/landseal/laptop.nix
@@ -35,18 +35,15 @@
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
       CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
 
       PLATFORM_PROFILE_ON_AC = "performance";
       PLATFORM_PROFILE_ON_BAT = "cool";
     };
   };
   # Nixos power saving
-  powerManagement = {
-    enable = true;
-    powertop.enable = true;
-  };
+  powerManagement.enable = true;
 
   # Enable networking
   networking = {
