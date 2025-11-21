ca70de64d6f52d82adfde364d205a19ff210a223
CubeSeal <kenndesilva1@gmail.com>
2025-11-21 20:39:52 +1100

    Make toggle-wallpaper more robust:
    1. Use -a for swww img for all monitors.
    2. Add messages.
    3. Add tr.
    4. Make awk only take the first line.

--- .config/niri/toggle-wallpaper.sh
+++ .config/niri/toggle-wallpaper.sh
@@ -3,10 +3,12 @@
 ANIMATED_WALLPAPER_PATH="$HOME/Wallpapers/Archlefirth.gif"
 STATIC_WALLPAPER_PATH="$HOME/Wallpapers/Archlefirth.png"
 
-CURRENT_WALLPAPER=$(swww query | awk -F'image: ' '{print $2}')
+CURRENT_WALLPAPER=$(swww query | awk -F'image: ' 'NR == 1 {print $2}')
 
 if [[ "$CURRENT_WALLPAPER" == "$STATIC_WALLPAPER_PATH" ]]; then
-    swww img "$ANIMATED_WALLPAPER_PATH"
+    swww img -a "$ANIMATED_WALLPAPER_PATH"
+    printf "Now displaying animated wallpaper.\n"
 else
-    swww img "$STATIC_WALLPAPER_PATH"
+    swww img -a "$STATIC_WALLPAPER_PATH"
+    printf "Now displaying static wallpaper.\n"
 fi
