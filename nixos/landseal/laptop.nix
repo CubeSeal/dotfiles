8f82bf855003a520375dc994fbf5362b464e6009
CubeSeal <kenndesilva1@gmail.com>
2025-11-22 15:34:27 +1100

    Change to balanced platform power profile.

--- nixos/landseal/laptop.nix
+++ nixos/landseal/laptop.nix
@@ -63,7 +63,7 @@
       CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
 
       PLATFORM_PROFILE_ON_AC = "performance";
-      PLATFORM_PROFILE_ON_BAT = "cool";
+      PLATFORM_PROFILE_ON_BAT = "balanced";
     };
   };
 
