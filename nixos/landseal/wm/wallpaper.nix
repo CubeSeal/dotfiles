550355c8fb875d4b47e7984d301ff2128841c956
CubeSeal <kenndesilva1@gmail.com>
2025-12-01 21:23:19 +1100

    Add swww as handler for static wallpaper.

--- nixos/landseal/wm/wallpaper.nix
+++ nixos/landseal/wm/wallpaper.nix
@@ -3,6 +3,8 @@
 {
    environment.systemPackages = with pkgs; [ 
     mpvpaper
+    swww
+    ffmpeg
     socat
   ];
 }
--- scripts/toggle-wallpaper.sh
+++ scripts/toggle-wallpaper.sh
@@ -1,16 +1,52 @@
 #!/usr/bin/env bash
+#Requirements: mpvpaper, ffmpeg, swww
 
 # === CONFIGURATION ===
 # IMPORTANT: Use an MP4 for the animation to save CPU/Battery
 WALLPAPER_PATH="$HOME/Wallpapers/rainy-night-city.3840x2160.mp4"
-SOCKET="/tmp/mpvpaper_socket"
+# Using robust extension stripping (works for .mp4, .mkv, etc)
+STATIC_PATH="${WALLPAPER_PATH%.*}.png"
+
+function start_swww_daemon {
+    if ! pgrep -x "swww-daemon" > /dev/null; then
+        printf "swww is not running. Starting swww...\n"
+        swww-daemon &
+        sleep 1
+    fi
+}
+
+if [ ! -f "$WALLPAPER_PATH" ]; then
+    printf "Wallpaper video not found at $WALLPAPER_PATH\n"
+    exit 1
+fi
 
 if [[ $1 == '--start' ]]; then
-    # Start mpvpaper in video mode on login
-    mpvpaper -pf -o "--panscan=1.0 input-ipc-server=$SOCKET no-audio --loop-file=inf --hwdec=auto" ALL "$WALLPAPER_PATH"
+
+
+    if [ ! -f "$STATIC_PATH" ]; then
+        printf "Creating static wallpaper image from video...\n"
+        ffmpeg -y -i "$WALLPAPER_PATH" -vframes 1 -f image2 "$STATIC_PATH" > /dev/null 2>&1
+    fi
+
+    start_swww_daemon
+    
     exit 0
+
 elif [[ $1 == '--toggle' ]]; then
-    echo "{\"command\": [\"cycle\", \"pause\"]}" | socat - $SOCKET 2>&1 > /dev/null
+    
+    # We use -f to match the filename to ensure we don't kill unrelated instances
+    if pgrep -f "mpvpaper.*$WALLPAPER_PATH" > /dev/null; then
+        printf "Stopping animated wallpaper...\n"
+        start_swww_daemon
+        pkill mpvpaper
+    else
+        printf "Starting animated wallpaper...\n"
+        # Using the optimized flags (GPU usage + Fill screen)
+        mpvpaper -pf -o "no-audio --loop-file=inf --hwdec=auto --panscan=1.0" ALL "$WALLPAPER_PATH"
+        sleep 1
+        pkill swww-daemon
+    fi
+
     exit 0
 else
     echo "Usage: $0 [--start|--toggle]"
